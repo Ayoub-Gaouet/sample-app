5758642e641a9e7059c69f46ef2bb7c5d66a4acc
Ayoub Gaouet <96016886+Ayoub-Gaouet@users.noreply.github.com>
2023-12-03 18:34:14 +0100

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -11,9 +11,9 @@ sudo usermod -aG docker jenkins
 set -e
 
 # Your existing script
-mkdir tempdir
-mkdir tempdir/templates
-mkdir tempdir/static
+mkdir -p tempdir
+mkdir -p tempdir/templates
+mkdir -p tempdir/static
 
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
