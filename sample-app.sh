74a9181c6242bd0995f1a0d4faa3099344f3de1e
Ayoub Gaouet <96016886+Ayoub-Gaouet@users.noreply.github.com>
2023-12-03 17:02:43 +0100

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-mkdir -p tempdir
-mkdir -p tempdir/templates
-mkdir -p tempdir/static
-
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
 
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
