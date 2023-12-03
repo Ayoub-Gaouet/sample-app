403eccec4542ccd51075402d16b8bbdef23cc045
Ayoub Gaouet <96016886+Ayoub-Gaouet@users.noreply.github.com>
2023-12-03 15:24:32 +0100

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-# Create directories if they don't exist
-mkdir -p tempdir
-mkdir -p tempdir/templates
-mkdir -p tempdir/static
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
 
-# Copy files
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-# Write to Dockerfile
-echo "FROM python" > tempdir/Dockerfile
+echo "FROM python" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
-echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
-# Build and run Docker container
 cd tempdir
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
-docker ps -a
+docker ps -a 
